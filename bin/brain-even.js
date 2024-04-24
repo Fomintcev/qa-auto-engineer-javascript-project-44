97b0305fc30812c0500b8fed0c66a73259617523
Roman Fomintcev <fomintcev.r@yandex.ru>
2024-04-25 00:25:40 +0300

    rename isEven function

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import isEven from '../src/games/runEvenGame.js';
+import evenGame from '../src/games/runEvenGame.js';
 import gameLogic from '../src/runGameLogic.js';
 
-gameLogic(isEven);
+gameLogic(evenGame);
